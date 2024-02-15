9ed9e9d6794507550f56b3c7513831473d6b8056
MelodicTechno <617179025@qq.com>
2024-02-15 21:11:06 +0800

    Create MazePanel.java

---
+++ src/MazePanel.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+import java.io.Serial;
+
+// the panel to contain the maze gui
+public class MazePanel extends JPanel {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+}
