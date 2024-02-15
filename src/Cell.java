48f7c2bf43621ecff7d612dc3c00817949dd1b15
MelodicTechno <617179025@qq.com>
2024-02-15 21:11:21 +0800

    Create Cell.java

---
+++ src/Cell.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.io.Serial;
+
+public class Cell extends JTextField {
+    @Serial
+    private static final long serialVersionUID = 1L;
+    // the data of cells should be described with a matrix
+    private int row;
+    private int column;
+}
