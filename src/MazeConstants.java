180f760d9a18c18c4dca37c7adf6d05c8e717227
MelodicTechno <617179025@qq.com>
2024-02-15 21:11:23 +0800

    Create MazeConstants.java

---
+++ src/MazeConstants.java
@@ -0,0 +1,8 @@
+// a class containing some constants for a universal usage
+public class MazeConstants {
+    // the size of each cell in the maze
+    public static final int CELL_SIZE = 1;
+    // the length of the maze
+    public static final int MAZE_SIZE = 40;
+
+}
