0711883341b41bdd0e821278cee4843cde200def
MelodicTechno <617179025@qq.com>
2024-02-17 15:34:38 +0800

    Update MazeConstants.java

--- src/MazeConstants.java
+++ src/MazeConstants.java
@@ -1,3 +1,7 @@
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+
 // a class containing some constants for a universal usage
 public class MazeConstants {
     // the size of each cell in the maze
@@ -5,4 +9,9 @@ public class MazeConstants {
     // the length of the maze
     public static final int MAZE_SIZE = 40;
 
+    // the borders
+    Border rightBorder = BorderFactory.createMatteBorder(0, 0, 0, 1, Color.BLACK);
+    Border topBorder = BorderFactory.createMatteBorder(1, 0, 0, 0, Color.BLACK);
+    Border bottomBorder = BorderFactory.createMatteBorder(0, 0, 1, 0, Color.BLACK);
+    Border leftBorder = BorderFactory.createMatteBorder(0, 1, 0, 0, Color.BLACK);
 }
