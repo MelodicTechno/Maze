ff0264f034d00663597113f94847c096b0bb4c91
MelodicTechno <617179025@qq.com>
2024-02-17 15:34:36 +0800

    Update Cell.java

--- src/Cell.java
+++ src/Cell.java
@@ -7,4 +7,21 @@ public class Cell extends JTextField {
     // the data of cells should be described with a matrix
     private int row;
     private int column;
+
+    // if it is connected with other cells
+    private boolean IsConnectedUp;
+    private boolean IsConnectedDown;
+    private boolean IsConnectedLeft;
+    private boolean IsConnectedRight;
+
+    // constructor
+    public Cell(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    // using the boarder to show if it is opened
+    public void makeBoarder() {
+
+    }
 }
