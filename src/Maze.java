8f8e89024d6ced0416d8307fc3a3b44ff75cc510
MelodicTechno <617179025@qq.com>
2024-02-17 23:27:16 +0800

    Update Maze.java

--- src/Maze.java
+++ src/Maze.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.io.Serial;
 import java.util.List;
 
@@ -9,8 +10,11 @@ public class Maze extends JPanel {
     private final List<Cell> cells;
     private final List<List<Boolean>> isConnected;
     public Maze() {
+        super.setLayout(new GridLayout(MazeConstants.MAZE_SIZE, MazeConstants.MAZE_SIZE));
+        super.setPreferredSize(new Dimension(MazeConstants.MAZE_SIZE * MazeConstants.CELL_SIZE,
+                MazeConstants.MAZE_SIZE * MazeConstants.CELL_SIZE));
         MazePuzzle mazePuzzle = new MazePuzzle();
-        mazePuzzle.eraseEdges(0.7);
+        mazePuzzle.eraseEdges(0.1);
         Graph<Cell, Boolean> mazeGraph = mazePuzzle.getMazeGraph();
         this.cells = mazeGraph.getVertices();
         this.isConnected = mazeGraph.getEdges();
@@ -21,9 +25,9 @@ public class Maze extends JPanel {
     }
 
     public void drawLines() {
-        for (int row = 0; row < MazeConstants.MAZE_SIZE; row++) {
-            for (int column = 0; column < MazeConstants.MAZE_SIZE; column++) {
-                if (!isConnected.get(row).get(column)) {
+        for (int row = 0; row < MazeConstants.VERTEX_NUMBER; row++) {
+            for (int column = 0; column < MazeConstants.VERTEX_NUMBER; column++) {
+                if (row != column && !isConnected.get(row).get(column)) {
                     Cell cell1 = this.cells.get(row);
                     Cell cell2 = this.cells.get(column);
                     if (cell1.isNeighborTo(cell2)) {
