7f5d91b105555cb3c769fdaa1dcf0b46e4ba196c
MelodicTechno <617179025@qq.com>
2024-02-17 23:27:20 +0800

    Update MazePuzzle.java

--- src/MazePuzzle.java
+++ src/MazePuzzle.java
@@ -1,3 +1,4 @@
+import java.util.HashSet;
 import java.util.Random;
 
 /**
@@ -9,9 +10,13 @@ public class MazePuzzle {
         int size = MazeConstants.VERTEX_NUMBER;
         Boolean[][] isConnected = new Boolean[size][size];
         Cell[] cells = new Cell[size];
+        for (int row = 0; row < MazeConstants.MAZE_SIZE; row++) {
+            for (int column = 0; column < MazeConstants.MAZE_SIZE; column++) {
+                cells[40 * row + column] = new Cell(row, column);
+            }
+        }
         for (int row = 0; row < size; row++) {
             for (int column = 0; column < size; column++) {
-                cells[10 * row + column] = new Cell(row, column);
                 isConnected[row][column] = true;
             }
         }
@@ -24,11 +29,22 @@ public class MazePuzzle {
     // erase the edges
     public void eraseEdges(double extent) {
         Random randomIndex = new Random();
-        int eraseIndex1 = randomIndex.nextInt(MazeConstants.MAZE_SIZE);
-        int eraseIndex2 = randomIndex.nextInt(MazeConstants.MAZE_SIZE);
+        HashSet<Integer> addedIndexes = new HashSet<>();
+
         int eraseTimes = (int) (MazeConstants.VERTEX_NUMBER * extent);
-        for (int time = 0; time < eraseTimes; time++) {
+        System.out.println("erase " + eraseTimes + " times" );
+        int times = 0;
+        while (times < eraseTimes) {
+            int eraseIndex1 = randomIndex.nextInt(0, MazeConstants.VERTEX_NUMBER);
+            int eraseIndex2 = randomIndex.nextInt(0, MazeConstants.VERTEX_NUMBER);
+            if (addedIndexes.contains(eraseIndex1) || addedIndexes.contains(eraseIndex2)) {
+                continue;
+            }
             mazeGraph.addEdge(eraseIndex1, eraseIndex2, false);
+            times++;
+            addedIndexes.add(eraseIndex1);
+            addedIndexes.add(eraseIndex2);
         }
+        System.out.println("all erasing finished");
     }
 }
