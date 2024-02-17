7b9738fe326bdf1e885d85333f21624f733cc93e
MelodicTechno <617179025@qq.com>
2024-02-17 23:27:13 +0800

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import com.formdev.flatlaf.FlatLightLaf;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.Serial;
+
+public class Main extends JFrame{
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public Main() {
+        Container cp = getContentPane();
+        cp.setLayout(new BorderLayout());
+        Maze maze = new Maze();
+        cp.add(maze);
+        pack();
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Maze");
+        setVisible(true);
+        System.out.println("everything is done");
+    }
+    public static void main(String[] args) {
+        // 更改界面风格
+        try {
+            UIManager.setLookAndFeel( new FlatLightLaf() );
+        } catch( Exception ex ) {
+            System.err.println( "Failed to initialize theme. Using fallback." );
+        }
+        SwingUtilities.invokeLater(Main::new);
+    }
+}
