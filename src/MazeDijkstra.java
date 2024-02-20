eb03feeb29a24d2055c49c2db3c557fd6e844f6a
MelodicTechno <617179025@qq.com>
2024-02-20 11:45:54 +0800

    Finish dijkstra

--- src/MazeDijkstra.java
+++ src/MazeDijkstra.java
@@ -1,16 +1,13 @@
 import java.util.*;
 
 public class MazeDijkstra {
-    private final MazePuzzle mazePuzzle;
-    private final ListGraph<Cell> graph;
     private final HashMap<Cell, Integer> distance = new HashMap<>();
     private final PriorityQueue<Cell> priorityQueue = new PriorityQueue<>();
-    private Set<Cell> settled;
-    private Cell[][] cells;
+    private final Set<Cell> settled;
+    private final Cell[][] cells;
     private final Map<Cell, List<Cell>> adjacencyList;
     public MazeDijkstra(MazePuzzle mazePuzzle) {
-        this.mazePuzzle = mazePuzzle;
-        this.graph = mazePuzzle.getMazeGraph();
+        ListGraph<Cell> graph = mazePuzzle.getMazeGraph();
         this.adjacencyList = graph.getAdjacencyList();
         this.settled = new HashSet<>();
         this.cells = mazePuzzle.getCells();
@@ -36,10 +33,29 @@ public class MazeDijkstra {
                 continue;
             }
             settled.add(newCell);
+
+            eNeighbors(newCell);
         }
     }
 
-    public MazePuzzle getMazePuzzle() {
-        return mazePuzzle;
+    private void eNeighbors(Cell newCell) {
+        int edgeDistance;
+        int newDistance;
+        // all the neighbors of Cell newCell
+        for (int i = 0; i < adjacencyList.get(newCell).size(); i++) {
+            Cell destinyCell = adjacencyList.get(newCell).get(i);
+            // If the destinyCell hasn't been processed
+            if (!settled.contains(destinyCell)) {
+                edgeDistance = destinyCell.getCost();
+                newDistance = distance.get(newCell) + edgeDistance;
+
+                // If the newDistance is cheaper
+                if (newDistance < edgeDistance) {
+                    distance.put(destinyCell, newDistance);
+                    destinyCell.setCost(newDistance);
+                }
+                priorityQueue.add(destinyCell);
+            }
+        }
     }
 }
